12de514c387d8e9442035d1192e96a02f3858076
Eri4ka <navyhasanov@gmail.com>
2023-09-13 00:17:13 +0500

    add jest, react-testing-library

--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -3,6 +3,7 @@ import cls from './SideBar.module.scss';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { useState } from 'react';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import { Button } from 'shared/ui/Button';
 
 interface SideBarProps {
   className?: string;
@@ -17,11 +18,14 @@ export const SideBar = ({ className }: SideBarProps) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.SideBar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button onClick={onToggle}>toggle</button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        toggle
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
